4f7c2bf66266cff9bd7be2e6fd5bfb9ef9d4b28e
Javier <javierbelbruno@gmail.com>
2021-05-23 15:32:24 -0300

    dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
-    path: resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'docs'),
   },
   
   resolve: {
